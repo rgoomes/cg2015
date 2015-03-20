08fe19d1141618681251bf72eb069ba4f6b0600a
rgoomes <ricardo.jpg@hotmail.com>
2015-03-20 12:11:44 +0000

    Testing suzanne

--- main.hpp
+++ main.hpp
@@ -3,7 +3,7 @@
 
 GLfloat r,g,b;
 
-Object cube("tests/cube.obj");
+Object cube("tests/suzanne.obj");
 
 void time_update(int value){
 	glutTimerFunc(50, time_update, 0);
@@ -12,9 +12,11 @@ void time_update(int value){
 
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+	glRotatef(2, 0, 1, 0);
 	for(int i = 0; i < cube.out_vertices.size(); i += 3){
-		glBegin(GL_POLYGON);
+		glBegin(GL_TRIANGLES);
 			glVertex3f(cube.out_vertices[i+0].x, cube.out_vertices[i+0].y, cube.out_vertices[i+0].z);
 			glVertex3f(cube.out_vertices[i+1].x, cube.out_vertices[i+1].y, cube.out_vertices[i+1].z);
 			glVertex3f(cube.out_vertices[i+2].x, cube.out_vertices[i+2].y, cube.out_vertices[i+2].z);
@@ -30,23 +32,22 @@ void load_objects(){
 
 void opengl_init(int argc, char **argv){
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 	glutCreateWindow("cg2015");
 
 	load_objects();
-	glClearColor(0.3,0.4,0.5, 1);
 	
 	glMatrixMode(GL_PROJECTION);
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 	gluPerspective(90, SCREEN_WIDTH/SCREEN_HEIGHT, 0, 10);
 	glMatrixMode(GL_MODELVIEW);
 	gluLookAt(5,5,5, 0,0,0, 0,1,0);
-	glShadeModel(GL_SMOOTH);
 
 	/////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////
 
+	glClearColor(0.3,0.4,0.5, 1);
 	glutDisplayFunc(display);
 	glutTimerFunc(1, time_update, 0);
 }
