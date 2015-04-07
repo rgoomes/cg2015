cec8a91ee2fa08a642386aab23becdb06d531e2f
Miguel Duarte <miguel.duarte20@hotmail.com>
2015-04-07 09:56:05 +0100

    Readds objects folder

---
+++ build/objects/textureVertexShader.glsl
@@ -0,0 +1,24 @@
+#version 120
+
+// Input vertex data, different for all executions of this shader.
+attribute vec3 vertexPosition_modelspace;
+attribute vec2 vertexUV;
+
+// Output data ; will be interpolated for each fragment.
+varying vec2 UV;
+varying vec3 temp;
+
+// Values that stay constant for the whole mesh.
+uniform mat4 MVP;
+uniform vec3 translate;
+
+void main(){
+
+	// Output position of the vertex, in clip space : MVP * position
+	temp = vertexPosition_modelspace + translate;
+	gl_Position =  MVP * vec4(temp,1);
+	
+	// UV of the vertex. No special space for this one.
+	UV = vertexUV;
+}
+
