6ba6054a4a0da7c33ca12187d956ed363111d703
rgoomes <ricardo.jpg@hotmail.com>
2015-04-13 13:26:33 +0100

    Implemented camera view

--- display.cpp
+++ display.cpp
@@ -1,6 +1,8 @@
 
 #include "display.hpp"
 
+#define PI 3.14159
+
 Rigidbody colorCube("objects/cube", 1, btVector3(10, 30, 0));
 Object dei("objects/dei");
 Rigidbody chair("objects/chair", 10, btVector3(20, 30, 20));
@@ -9,6 +11,10 @@ float mvp[4][4];
 float a=0;
 GLFWwindow* window;
 btDynamicsWorld* world;
+
+double ray=5.0, ang=0.5*PI+90, inc=100;
+double obs_ini[] = {60,18,-10};
+double obs_fin[] = {obs_ini[0]-ray*cos(ang), obs_ini[1], obs_ini[2]-ray*sin(ang)};
 
 void set_environment(GLFWwindow* _window, btDynamicsWorld* _world){
 	window = _window;
@@ -32,11 +38,8 @@ void load_objects(){
 }
 
 void add_lights(){
-	glDepthFunc(GL_LESS);
-
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
-
 }
 
 void get_mvp(float mvp[4][4]){
@@ -44,6 +47,26 @@ void get_mvp(float mvp[4][4]){
 	glGetFloatv(GL_MODELVIEW_MATRIX, m1[0]);
 	glGetFloatv(GL_PROJECTION_MATRIX, m2[0]);
 	mult_matrix(mvp, m1, m2);
+}
+
+void camera_view(float elapsed){
+	if(glfwGetKey(window, GLFW_KEY_UP)   || glfwGetKey(window, GLFW_KEY_W)){
+		obs_ini[0] = obs_ini[0] + elapsed*inc*cos(ang);
+		obs_ini[2] = obs_ini[2] - elapsed*inc*sin(ang);
+	}
+	if(glfwGetKey(window, GLFW_KEY_DOWN)  || glfwGetKey(window, GLFW_KEY_S)){
+		obs_ini[0] = obs_ini[0] - elapsed*inc*cos(ang);
+		obs_ini[2] = obs_ini[2] + elapsed*inc*sin(ang);
+	}
+	if(glfwGetKey(window, GLFW_KEY_LEFT)  || glfwGetKey(window, GLFW_KEY_A))
+		ang = (ang + elapsed * 1.0);
+	if(glfwGetKey(window, GLFW_KEY_RIGHT) || glfwGetKey(window, GLFW_KEY_D))
+		ang = (ang - elapsed * 1.0);
+
+	obs_fin[0] = obs_ini[0] + ray * cos(ang);
+	obs_fin[2] = obs_ini[2] - ray * sin(ang);
+
+	gluLookAt(obs_ini[0],obs_ini[1],obs_ini[2], obs_fin[0],obs_fin[1],obs_fin[2], 0,1,0);
 }
 
 void display(float elapsed){
@@ -58,8 +81,10 @@ void display(float elapsed){
 	
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	a+=0.5*elapsed;
-	gluLookAt(80*sin(a)-30,40,80*cos(a), 30,-1,0, 0, 1, 0);
+	
+	// a+=0.5*elapsed;
+	// gluLookAt(80*sin(a)-30,40,80*cos(a), 30,-1,0, 0, 1, 0);
+	camera_view(elapsed);
 
 	GLfloat	lightpos[4] = {10.0, 20.0, 0.0, 1.0}; 
 	glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
