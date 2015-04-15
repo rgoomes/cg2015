adc92324ca60c461df1f1cabc586b681def0a16b
Miguel Duarte <miguel.duarte20@hotmail.com>
2015-04-15 16:47:48 +0100

    Render buffer

--- display.cpp
+++ display.cpp
@@ -80,9 +80,57 @@ void camera_view(float elapsed){
 
 GLfloat light_position[] = { 1.0, 15.0, -30.0, 1.0 };
 
+int get_render_buffer(){
+		// The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
+	GLuint FramebufferName = 0;
+	glGenFramebuffers(1, &FramebufferName);
+	glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+
+	// The texture we're going to render to
+	GLuint renderedTexture;
+	glGenTextures(1, &renderedTexture);
+	 
+	// "Bind" the newly created texture : all future texture functions will modify this texture
+	glBindTexture(GL_TEXTURE_2D, renderedTexture);
+	 
+	// Give an empty image to OpenGL ( the last "0" )
+	glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, 1024, 768, 0,GL_RGB, GL_UNSIGNED_BYTE, 0);
+	 
+	// Poor filtering. Needed !
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	if ( !GLEW_ARB_framebuffer_object ){ // OpenGL 2.1 doesn't require this, 3.1+ does
+		printf("Your GPU does not provide framebuffer objects. Use a texture instead.");
+		return -1;
+	}
+
+	// The depth buffer
+	GLuint depthrenderbuffer;
+	glGenRenderbuffers(1, &depthrenderbuffer);
+	glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 1024, 768);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderedTexture, 0);
+
+	// Set "renderedTexture" as our colour attachement #0
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderedTexture, 0);
+	 
+	// Set the list of draw buffers.
+	GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+	glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
+
+	// Always check that our framebuffer is ok
+	if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		printf("error\n");
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	return renderedTexture;
+}
+
 void display(float elapsed){
 	int w, h;
 	glfwGetWindowSize(window, &w, &h);
+
 
 	glClearColor(0.3,0.4,0.5, 1);
 
@@ -106,5 +154,26 @@ void display(float elapsed){
 	glTranslatef(20, 0, 0);
 	dei.render_ntexture();
 	
+
+	GLuint texture = get_render_buffer();
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glBegin(GL_TRIANGLES);
+		glTexCoord2f(0, 0);
+		glVertex3f(0, 30, 0);
+		glTexCoord2f(0, 1);
+		glVertex3f(0, 30, 10);
+		glTexCoord2f(1, 0);
+		glVertex3f(10, 30, 0);
+
+		glTexCoord2f(1, 1);
+		glVertex3f(10, 30, 10);
+		glTexCoord2f(0, 1);
+		glVertex3f(0, 30, 10);
+		glTexCoord2f(1, 0);
+		glVertex3f(10, 30, 0);
+	glEnd();
+	
+
 }
 
--- main.cpp
+++ main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char **argv){
 	load_objects();
 
 	last_tick = glfwGetTime();
-
+	
 	do{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
