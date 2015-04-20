9d562d371dae5a65c88083c456dbfdd5ff11b1b2
rgoomes <ricardo.jpg@hotmail.com>
2015-04-20 10:33:51 +0100

    Implemented phong

--- build/objects/textureFragmentShader.glsl
+++ build/objects/textureFragmentShader.glsl
@@ -5,6 +5,7 @@ varying vec2 UV;
 varying vec3 Normal_cameraspace;
 varying vec3 LightDirection_cameraspace;
 varying vec4 ShadowCoord;
+varying vec3 vertPos;
 
 // Values that stay constant for the whole mesh.
 uniform sampler2D myTextureSampler;
@@ -44,13 +45,21 @@ void main(){
 	// Material properties
 	vec3 MaterialDiffuseColor = texture2D( myTextureSampler, UV ).rgb;
 	vec3 MaterialAmbientColor = vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
+	vec3 MaterialSpecColor = vec3(1.0, 1.0, 1.0);
 
 	vec3 n = normalize(Normal_cameraspace);
 	vec3 l = normalize(LightDirection_cameraspace);
 	
 	float visibility=1.0;
 	float cosTheta = clamp( dot( n,l ), 0,1 );
-	cosTheta = clamp(dot( n,l ), 0,1 );
+
+	float specular = 0.0;
+	//if(cosTheta > 0){
+		vec3 viewDir = normalize(-vertPos);
+		vec3 halfDir = normalize(l + viewDir);
+		float specAngle = max(dot(halfDir, n), 0.0);
+		specular = pow(specAngle, 16.0);
+	// 	}
 
 	float bias = 0.005;
 	// ...variable bias
@@ -83,6 +92,7 @@ void main(){
 		// Ambient : simulates indirect lighting
 		MaterialAmbientColor +
 		// Diffuse : "color" of the object
-		MaterialDiffuseColor * LightColor * cosTheta;
+		MaterialDiffuseColor * LightColor * cosTheta + 
+		specular * MaterialSpecColor;
 
 }
--- build/objects/textureVertexShader.glsl
+++ build/objects/textureVertexShader.glsl
@@ -10,6 +10,7 @@ varying vec2 UV;
 varying vec4 ShadowCoord;
 varying vec3 LightDirection_cameraspace;
 varying vec3 Normal_cameraspace;
+varying vec3 vertPos;
 
 // Values that stay constant for the whole mesh.
 uniform mat4 MVP;
@@ -23,6 +24,9 @@ void main(){
 	gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
 	ShadowCoord = DepthBiasMVP * vec4(vertexPosition_modelspace,1);
 	
+	vec4 vertPos4 = MVP * vec4(vertexPosition_modelspace, 1.0);
+	vertPos = vec3(vertPos4) / vertPos4.w; 
+
 	LightDirection_cameraspace = (V * vec4(LightInvDirection_worldspace, 0)).xyz;
 	//Normal_cameraspace = (V * vec4(vertexNormal_modelspace, 0)).xyz;
 	Normal_cameraspace = (V * M * vec4(vertexNormal_modelspace, 0)).xyz;
